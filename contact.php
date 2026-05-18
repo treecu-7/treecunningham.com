23290871bd88e56e2f1bc5b0a6c09128523a883c
treecu-7 <eacunningham05@gmail.com>
2026-05-17 18:53:33 -0700

    fix4

--- contact.php
+++ contact.php
@@ -1,53 +1,42 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+    
+    $errors = array();
+    
+    if (empty($name)) {
+        $errors[] = "Name is required";
+    }
 
-session_start()
-
-$honeypot = filter_input(INPUT_POST, 'sname', FILTER_SANITIZA_STRING);
-
-if ($honeypot) {
-    header($_SERVER['SERVER_PROTOCOL'] . '405 Method Not Allowed');
-    exit;
-}
-
-$errors = [];
-
-if (!empty($_POST)) {
-   $name = $_POST['name'];
-   $email = $_POST['email'];
-   $subject = $_POST['subject'];
-   $message = $_POST['message'];
-  
-   if (empty($name)) {
-       $errors[] = 'Name is empty';
-   }
-
-   if (empty($email)) {
-       $errors[] = 'Email is empty';
-   } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-       $errors[] = 'Email is invalid';
-   }
-
-   if (empty($subject)) {
-       $errors[] = 'Subject is empty';
-   }
-
-   if (empty($message)) {
-       $errors[] = 'Message is empty';
-   }
-}
-
-return ['mail' => ['to_email' => 'eacunningham05@gmail.com']];
-
-if (isset($_POST['submit'])) {
-    $name = $_POST['name'];
-    $mailFrom = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
-
-    $mailTo = "eacunningham05@gmail.com";
-    $headers = "From: ".$mailFrom;
-    $txt = "You have received an e-mail from ".$.name.".\n\n".$message;
-
-    mail($mailTo, $subject, $txt, $headers);
+     if (empty($subject)) {
+        $errors[] = "Subject is required";
+    }
+    
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errors[] = "Invalid email format";
+    }
+    
+    if (empty($message)) {
+        $errors[] = "Message is required";
+    }
+    
+    if (empty($errors)) {
+        $to = "eacunningham05@gmail.com";
+        $subject = "$subject";
+        $headers = "From: $email";
+        
+        if (mail($to, $subject, $message, $headers)) {
+            echo "Thank you for contacting us!";
+        } else {
+            echo "Oops! Something went wrong.";
+        }
+    } else {
+        foreach ($errors as $error) {
+            echo $error . "<br>";
+        }
+    }
 }
 ?>
